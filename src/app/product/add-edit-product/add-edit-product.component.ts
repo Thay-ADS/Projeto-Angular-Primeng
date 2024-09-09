6cb586025e67f6637628b60ac543f99f4eefe76b
Jeremias Bezerra Lucas <88001432+JeremiasLucas@users.noreply.github.com>
2024-09-09 22:46:14 +0000

    :construction: CORS falhando

--- src/app/product/add-edit-product/add-edit-product.component.ts
+++ src/app/product/add-edit-product/add-edit-product.component.ts
@@ -1,25 +1,32 @@
 import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-add-edit-product',
   templateUrl: './add-edit-product.component.html',
-  styleUrl: './add-edit-product.component.css'
+  styleUrls: ['./add-edit-product.component.css']
 })
 export class AddEditProductComponent {
   name: string = 'Jeremaias';
   description: string = 'Pedreiro';
+  constructor(private http: HttpClient) {}
   
   @Input() displayAddModal=false;
   onSubmit(form: NgForm) {
     if(form.valid) {
-      alert();
+      const newData = { name: 'Jeremaias', description: 'Pedreiro' };
+      this.http.post<any>('https://3.128.249.166:8000/api/itens/', newData).subscribe((response) => {
+        console.log(response);
+        this.displayAddModal = false
+    }
+    );
     }
   }
 
-
   showAddModal(){
     this.displayAddModal=true;
+  }
 }
-}
+1
