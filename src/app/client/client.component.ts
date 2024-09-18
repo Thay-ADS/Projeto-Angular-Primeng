85037fa82f4272dd3981cff584b9b58acbc7c08f
JeremiasLucas <lucas.jeremias@escolar.ifrn.edu.br>
2024-09-17 21:40:59 -0300

    getClents() construído

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-client',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './client.component.css'
 })
 export class ClientComponent {
+  constructor(private http:HttpClient) { }
 
+  getClients(){
+    return this.http.get('http://3.128.249.166:8000/api/clients/');
+  }
 }
