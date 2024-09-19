2aca68d068ac5f89234550f2826dae6687a84776
Thay-ADS <143521309+Thay-ADS@users.noreply.github.com>
2024-09-19 11:27:24 -0300

    Update dashboard.component.ts

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -13,6 +13,18 @@ export class DashboardComponent {
       labels:["jan","fev","mar","abr","mai","jun","jul","ago","set","out","nov","dez"],
       datasets:[{
         labels:'2023',
-        data:[10,20,30,40,50,60,70,80,90,100,110,120]
-],      
+        data:[10,20,30,40,50,60,70,80,90,100,110,120],
+        backgoundColor:["red"],
+        borderColor:["black"],
+        borderWidth:1
+      },
+     {
+       labels:'2023',
+       data:[10,20,30,40,50,60,70,80,90,100,110,120],
+     }
+                  
+                
+]
+}
+}
 }
