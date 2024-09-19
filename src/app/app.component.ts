8e5748c2cde92c50f91c15d5989acdecfdb504fb
Thay-ADS <143521309+Thay-ADS@users.noreply.github.com>
2024-09-19 13:33:04 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,79 +15,13 @@ import { text } from 'stream/consumers';
 })
 export class AppComponent {
  
-  items:MenuItem[]|undefined;
-  
-  data:any;
-  options:any;
- 
-  pageSize = 10;  
-  currentpage = 2;
-  first:any;
+
   
  
   constructor() { }
 
   ngOnInit() {  
-    this.options={
-      plugins:{
-        title:{
-          display:true,
-          text:"Unique visitor graph"
-        }
-      },
-      elements:{
-        bar:{
-          backgroundColor:"black",
-          
-          
-          borderWidth: '1200 px',
-
-        },
-        point:{
-         
-          pointRadius:0,
-          
-        },
-        line:{
-         
-          stepped:true,
-        }
-      },
-      
-    };
-    this.items=[
-      {
-        label :"Dashboards",
-        
-        items:[
-          {label:"E-Commerce", icon: "pi pi-fw pi-home",routerLink:"/"},
-          
-        ]
-      },
-     
-      {
-        label:"Itens", 
-        items:[
-          
-          {label:"Visualizar",routerLink:"/produtos",
-          icon: "pi pi-fw pi-comment",
-          
-        },
-
-        ]
-      },
-      {
-        label:"Clientes",
-        
-        items:[
-          
-          {label:"Visualizar",routerLink:"/clientes",
-          icon: "pi pi-fw pi-comment",
-          
-        },
-        
-        ]
-      },
-    ]
+    
+  
   }
 }
