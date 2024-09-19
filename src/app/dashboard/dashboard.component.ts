2cfbe1b55f9287ff8d600750b15817b498b8236a
Thay-ADS <143521309+Thay-ADS@users.noreply.github.com>
2024-09-19 12:42:18 -0300

    Update dashboard.component.ts

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class DashboardComponent {
   basicData:any;
   basicOptions:any;
-  siderVisible=false;
+  sidebarVisible=false;
   
 
   ngOnInit(){
